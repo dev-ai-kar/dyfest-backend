b29e80f0f50e2803bdd97e20164e8a16f91e7504
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2021-02-17 18:31:44 +0530

    Protected Routes

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     path('', views.getRoutes, name= "routes"),
+
     path('users/profile/', views.getUserProfile, name="user-profile"),
+    path('users/', views.getUsers, name="user"),
+
     path('events/', views.getEvents, name="events"),
     path('events/<str:pk>/', views.getEvent, name="event"),
 ]
