4824daec3ed942494fe606b080a4ea14255797f1
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2021-02-17 17:06:38 +0530

    JWT Customization

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name= "routes"),
     path('events/', views.getEvents, name="events"),
     path('events/<str:pk>/', views.getEvent, name="event"),
