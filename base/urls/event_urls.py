19108f5c2a806cba28dd6dc442cbe9fc882f927d
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2021-03-16 00:15:26 +0530

    Event Edit Screen

--- base/urls/event_urls.py
+++ base/urls/event_urls.py
@@ -3,7 +3,10 @@ from base.views import event_views as views
 
 urlpatterns = [
     path('', views.getEvents, name="events"),
+
+    path('create/', views.createEvent, name="event-create"),
     path('<str:pk>/', views.getEvent, name="event"),
 
+    path('update/<str:pk>/', views.deleteEvent, name="event-update"),
     path('delete/<str:pk>/', views.deleteEvent, name="event-delete"),
 ]
--- base/views/event_views.py
+++ base/views/event_views.py
@@ -23,6 +23,40 @@ def getEvent(request, pk):
     serializer = EventSerializer(event, many= False) # many = True # serializing many object or one
     return Response(serializer.data)
 
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createEvent(request):
+    user=request.user
+    event = Event.objects.create(
+        user=user, 
+        name = 'Sample Name',
+        price = 0,
+        brand = 'Sample Brand',
+        countInStock = 0,
+        category ='Sample Category',
+        description = 'Sample Description'
+    )
+    serializer = EventSerializer(event, many= False) # many = True # serializing many object or one
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def updateEvent(request, pk):
+    data = request.data
+    event = Event.objects.get(_id = pk)
+    event.name = data['name']
+    event.brand = data['brand']
+    event.category = data['category']
+    event.countInStock = data['countInStock']
+    event.description = data['description']
+
+    event.save()
+
+    serializer = EventSerializer(event, many= False) # many = True # serializing many object or one
+    return Response(serializer.data)
+
+
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def deleteEvent(request, pk):
