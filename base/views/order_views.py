9bc663292f395f762b7991e6765ec9f9a944ebb0
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2021-03-16 12:39:21 +0530

    Mark Order as Payed & Delivered

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -89,10 +89,19 @@ def getOrderById(request, pk):
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
         
 @api_view(['PUT'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAdminUser]) #IsAuthenticated
 def updateOrderToPaid(request, pk):
     order = Order.objects.get(_id=pk)
     order.isPaid = True
     order.paidAt = datetime.now()
     order.save()
     return Response('Order was paid')
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateOrderToDelivered(request, pk):
+    order = Order.objects.get(_id=pk)
+    order.isDelivered = True
+    order.deliveredAt = datetime.now()
+    order.save()
+    return Response('Order was delivered')
